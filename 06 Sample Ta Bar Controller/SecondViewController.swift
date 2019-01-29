571ca7bd0440f2c5b420e528a242e37ecff1fbec
Gustavo Ureta <5447557+ginogustavo@users.noreply.github.com>
2019-01-28 21:54:52 -0500

    Initial Commit

---
+++ 06 Sample Ta Bar Controller/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  06 Sample Ta Bar Controller
+//
+//  Created by AcademiamovilesAir on 1/28/19.
+//  Copyright © 2019 index. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
